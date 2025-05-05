36bf3cd4e556eab4b8d6d57795fe0fe48f8e42b8
JoaoPauloHJ <joaopaulohj126@hotmail.com>
2025-05-05 15:04:51 -0300

    Juntando os sites

---
+++ script.js
@@ -0,0 +1,84 @@
+//Todas as funções do site
+function onChangeEmail() {
+    toggleButtonDisable();
+    toggleEmailErrors();
+}
+
+function onChangePassword() {
+    toggleButtonDisable();
+    togglePasswordErros();""
+}
+
+const form = {
+    email: () => document.getElementById('email'),
+    emailValidError: () => document.getElementById('email-invalid-error'),
+    emailRequiredError: () => document.getElementById('email-required-error'),
+    loginButton: () => document.getElementById('login-button'),
+    password: () => document.getElementById('password'),
+    passwordRequiredError: () => document.getElementById('password-required-error'),
+    recoverPassword: () => document.getElementById('recover-password-button')
+
+} 
+
+function login() {
+    showLoading();
+    firebase.auth().signInWithEmailAndPassword(
+        form.email().value, form.password().value
+    ).then(response => {
+        hideLoading();
+        window.location.href = "principal.html"
+    }).catch(error => {
+        hideLoading();
+        alert(getErrorMessage(error));
+    });
+}
+
+function getErrorMessage(error) {
+    if (error.code == "auth/user-not/found") {
+        return "Usuário nao encontrado";
+    }
+    return error.menssage
+}
+
+function register() {
+    window.location.href = "Paginas/Registro/registroFinan.html"
+}
+
+function isEmailValid() {
+    const email = form.email().value;
+    if (!email) {
+        return false;
+    }
+    return validateEmail(email);
+}
+
+function toggleEmailErrors() {
+    const email = form.email().value;
+
+    form.emailRequiredError().style.display = email ? "none" : "block";
+
+    form.emailValidError().style.display = validateEmail(email) ? "none" : "block";
+}
+
+function togglePasswordErros() {
+    const password = form.password().value;
+
+    form.passwordRequiredError().style.display = password ? "none" : "block";
+}
+
+function toggleButtonDisable() {
+    const emailValid = isEmailValid();
+    form.recoverPassword().disabled = !emailValid;
+
+    const passwordValid = isPasswordValid();
+    form.loginButton().disabled = !emailValid || !passwordValid;
+}
+
+function isPasswordValid() {
+    const password = form.password().value;
+    if (!password) {
+        return false;
+    }
+    return true;
+}
+
